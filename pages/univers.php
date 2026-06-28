c86ca9c2690d78fac256bc5f39735f9f0323accf
Rika600 <karima740@icloud.com>
2026-06-28 21:09:08 +0200

    feat: page univers complète + CSS filtre blanc + cartes lieux 2 par ligne

--- pages/univers.php
+++ pages/univers.php
@@ -13,16 +13,15 @@ $personnages = $personnageService->getPersonnages();
 $lieux = $lieuService->getLieux();
 ?>
 <div class="container my-5">
-    <h1 class="text-center mb-5">L'Univers</h1>
+    <h1 class="text-center mb-3">L'Univers</h1>
 
-    <!-- Bouton toggle filtres -->
-    <div class="d-flex align-items-center mb-4">
-        <button id="btn-toggle-filtres" class="btn p-0 me-3" aria-label="Ouvrir les filtres">
+    <!-- Bouton filtre en haut à gauche -->
+    <div class="mb-4">
+        <button id="btn-toggle-filtres" class="btn p-0" aria-label="Ouvrir les filtres">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </button>
-        <h2 class="m-0">Personnages & Lieux</h2>
     </div>
 
     <!-- Panneau filtres -->
@@ -53,10 +52,23 @@ $lieux = $lieuService->getLieux();
         </div>
     </div>
 
-    <!-- Grille des fiches -->
+    <!-- Portrait maudit -->
+    <div class="text-center mb-5">
+        <img src="<?= BASE_URL ?>images/portrait_maudit.png" 
+             alt="Le Portrait Maudit" 
+             class="img-fluid portrait-maudit">
+    </div>
+
+    <!-- Planche personnages principaux -->
+    <div class="text-center mb-5">
+        <img src="<?= BASE_URL ?>images/planche_principaux.png" 
+             alt="Personnages principaux" 
+             class="img-fluid w-100">
+    </div>
+
+    <!-- Grille des fiches cliquables -->
     <div class="row" id="fiches-grid">
 
-        <!-- Personnages -->
         <?php foreach ($personnages as $p) : ?>
         <div class="col-md-4 mb-4">
             <div class="card h-100 border-0">
@@ -74,10 +86,9 @@ $lieux = $lieuService->getLieux();
         </div>
         <?php endforeach; ?>
 
-        <!-- Lieux -->
         <?php foreach ($lieux as $l) : ?>
-        <div class="col-md-4 mb-4">
-            <div class="card h-100 border-0">
+        <div class="col-md-6 mb-4">
+            <div class="card border-0">
                 <img src="<?= BASE_URL ?>images/<?= htmlspecialchars($l->getImage()) ?>"
                      alt="<?= htmlspecialchars($l->getNom()) ?>"
                      class="card-img-top fiche-image">
@@ -93,6 +104,21 @@ $lieux = $lieuService->getLieux();
         <?php endforeach; ?>
 
     </div>
+
+    <!-- Planche personnages secondaires -->
+    <div class="text-center mt-5 mb-5">
+        <img src="<?= BASE_URL ?>images/planche_secondaires.png" 
+             alt="Personnages secondaires" 
+             class="img-fluid w-100">
+    </div>
+
+    <!-- Planche des lieux -->
+    <div class="text-center mt-3 mb-5">
+        <img src="<?= BASE_URL ?>images/planche_lieux.png" 
+             alt="L'Univers de Dorian Gray - Les Lieux" 
+             class="img-fluid w-100">
+    </div>
+
 </div>
 
 <script>var BASE_URL = '<?= BASE_URL ?>';</script>
